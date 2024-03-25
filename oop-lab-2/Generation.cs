1bcc6ca676f269c74c0866fdd98805e11291369f
Alice Kolomiets <85138300+glamiturianmai@users.noreply.github.com>
2024-03-26 01:22:07 +0300

    this!!!! (lab-2)

---
+++ oop-lab-2/Generation.cs
@@ -0,0 +1,93 @@
+﻿namespace oop_lab_2
+{
+    public class Generation
+    {
+        public int a;
+        public int b;
+        public int c;
+        public int d;
+        public static Random r = new Random();
+        public int ex_res; //ожидаемый результат 
+        public Func<int, int, int, int, int> f;
+        public int? real_res; //реальный результат
+
+        public int RealResult
+        {
+            get
+            {
+                if (real_res == null)
+                {
+                    real_res = f(a, b, c, d);
+                }
+
+                return (int)real_res;
+            }
+        }
+
+        public Generation(Func<int, int, int, int, int> f, int a, int b, int c, int d, int ex_res)
+        {
+            this.f = f;
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+            this.ex_res = ex_res;
+
+        }
+
+        //механизм наследования
+        public static Generation NewGen(Generation p1, Generation p2)
+        {
+            int x = r.Next(1, 4);
+            switch (x)
+            {
+                case 1: return new Generation(p1.f, p1.a, p1.b, p2.c, p2.d, p2.ex_res);
+                case 2: return new Generation(p1.f, p1.a, p2.b, p2.c, p2.d, p2.ex_res);
+                case 3: return new Generation(p1.f, p1.a, p1.b, p1.c, p2.d, p2.ex_res);
+                default: return null;
+
+            }
+        }
+
+        //мутация
+        public void Change()
+        {
+            int imposter = r.Next(1, 5);
+            switch (imposter)
+            {
+                case 1:
+                    a = r.Next(1, 30);
+                    break;
+                case 2:
+                    b = r.Next(1, 30);
+                    break;
+                case 3:
+                    c = r.Next(1, 30);
+                    break;
+                case 4:
+                    d = r.Next(1, 30);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        //На сколько реальный результат отклонился от ожидаемого 
+        public int GetAc()
+        {
+            return Math.Abs((int)(ex_res - RealResult));
+        }
+
+        //средний коэф. выживания поколения 
+        public static double SrKoef(List<Generation> generations)
+        {
+            double sum = 0;
+            foreach (Generation item in generations)
+            {
+                sum += item.GetAc();
+            }
+            return sum / generations.Count();
+        }
+
+    }
+}
---
+++ oop-lab-2/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace oop_lab_2
+{
+    public class Program
+    {
+        public static int res = 30;
+        public static Func<int, int, int, int, int> f = (a, b, c, d) => a + 2 * b + 3 * c + 4 * d;
+        public static Random r = new Random();
+
+        static void Main(string[] args)
+        {
+            List<Generation> generators = new List<Generation>();
+            for (int i = 0; i < 5; i++)
+            {
+                generators.Add(new Generation(f, r.Next(1, 30), r.Next(1, 30), r.Next(1, 30), r.Next(1, 30), res));
+
+            }
+            for (int i = 0; ; i++)
+            {
+                Console.WriteLine("Итерация " + (i + 1));
+                foreach (var item in generators)
+                {
+                    Console.Write("a = " + item.a + " b = " + item.b + " c = " + item.c + " d = " + item.d + "\n");
+                }
+                Console.WriteLine("--------");
+
+                double koef1 = Generation.SrKoef(generators);
+                var best = generators
+                    .OrderBy(g => g.GetAc())
+                    .ToList();
+                if (best.Any(g => g.RealResult == res))
+                {
+                    Solution(best[0]);
+                    break;
+                }
+
+                generators.Clear();
+                generators.Add(Generation.NewGen(best[0], best[1]));
+                generators.Add(Generation.NewGen(best[1], best[0]));
+                generators.Add(Generation.NewGen(best[0], best[2]));
+                generators.Add(Generation.NewGen(best[2], best[0]));
+                generators.Add(Generation.NewGen(best[1], best[2]));
+
+                double koef2 = Generation.SrKoef(generators);
+                if (koef2 <= koef1)
+                {
+                    Random ran = new Random();
+                    int x = ran.Next(1, 5);
+                    for (int j = 0; j < x; j++)
+                    {
+                        generators[ran.Next(0, 5)].Change();
+                    }
+                }
+            }
+        }
+
+        public static void Solution(Generation g)
+        {
+            Console.WriteLine("Решение: ");
+            Console.Write("a=" + g.a + " b=" + g.b + " c=" + g.c + " d=" + g.d + "\n");
+        }
+
+    }
+}
